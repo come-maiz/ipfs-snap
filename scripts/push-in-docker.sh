b4972181d49c5b05e8124fa656729c788ecee7eb
Leo Arias <leo.arias@canonical.com>
2016-12-15 19:06:58 +0000

    Add the missing script

---
+++ scripts/push-in-docker.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#
+# Push a snap to the store using a docker container.
+
+set -ev
+
+docker run -v $(pwd):$(pwd) -t snapcore:snapcraft sh -c "apt update -qq && apt install snapcraft -y && cd $(pwd) && snapcraft push *.snap --release edge"
