6a9a8be8c3ab4bf22404471b0622965d5a678c20
Leo Arias <leo.arias@canonical.com>
2016-12-15 19:15:03 +0000

    Fix the image name

--- scripts/push-in-docker.sh
+++ scripts/push-in-docker.sh
@@ -4,4 +4,4 @@
 
 set -ev
 
-docker run -v $(pwd):$(pwd) -t snapcore:snapcraft sh -c "apt update -qq && apt install snapcraft -y && cd $(pwd) && snapcraft push *.snap --release edge"
+docker run -v $(pwd):$(pwd) -t snapcore/snapcraft sh -c "apt update -qq && apt install snapcraft -y && cd $(pwd) && snapcraft push *.snap --release edge"
