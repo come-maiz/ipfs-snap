b7cf83b2d5a80fda202d3f8355ce810b62bc694c
Leo Arias <leo.arias@canonical.com>
2017-06-29 04:57:16 +0000

    fix the snap script

--- scripts/snap.sh
+++ scripts/snap.sh
@@ -4,12 +4,14 @@
 
 set -ev
 
+snapcraft=/snap/bin/snapcraft
+
 # Check if the latest tag is in the beta channel.
 tmp_dir="$(mktemp -d)"
 source="$(cat snapcraft.yaml | grep source: | head -n 1 | awk '{printf $2}')"
 git clone "${source}" "${tmp_dir}"
 last_committed_tag="$(git -C "${tmp_dir}" describe --tags --abbrev=0)"
-snapcraft status ipfs || echo "none" > status
+$snapcraft status ipfs || echo "none" > status
 last_released_tag="$(cat status | grep beta | awk '{print $2}')"
 
 if [ "${last_committed_tag}" != "${last_released_tag}" ]; then
@@ -18,5 +20,5 @@ if [ "${last_committed_tag}" != "${last_released_tag}" ]; then
     sed -i "s/version:.*$/version: '"$last_committed_tag"'/g" snapcraft.yaml
 fi
 
-snapcraft clean
-snapcraft
+$snapcraft clean
+$snapcraft
