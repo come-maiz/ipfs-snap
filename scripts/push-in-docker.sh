8fb167ce52c9d1beea404696704d1299ccae0f4e
Leo Arias <yo@elopio.net>
2019-02-07 01:37:15 +0000

    Update the credentials and remove docker

--- scripts/push-in-docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#
-# Push a snap to the store using a docker container.
-
-set -ev
-
-docker run -v $(pwd):$(pwd) -v $HOME:/root/ -t snapcore/snapcraft sh -c "apt update -qq && apt install snapcraft -y && cd $(pwd) && ls *.snap && for file in *.snap; do echo 'file: ' \$file && snapcraft push \$file --release edge; done"
--- scripts/snap.sh
+++ scripts/snap.sh
@@ -1,8 +1,22 @@
 #!/bin/bash
 #
-# Build a snap.
+# Build the ipfs snaps.
 
 set -ev
+
+# Check if the latest tag is in the beta channel.
+tmp_dir="$(mktemp -d)"
+source="$(cat snapcraft.yaml | grep source: | head -n 1 | awk '{printf $2}')"
+git clone "${source}" "${tmp_dir}"
+last_committed_tag="$(git -C "${tmp_dir}" describe --tags --abbrev=0)"
+(snapcraft status ipfs || echo "none") > status
+last_released_tag="$(cat status | grep beta | head -n1 | awk '{print $2}')"
+
+if [ "${last_committed_tag}" != "${last_released_tag}" ]; then
+    # Build using the latest tag.
+    sed -i "0,/source-tag/s/source-tag:.*$/source-tag: '"$last_committed_tag"'/g" snapcraft.yaml
+    sed -i "s/version:.*$/version: '"$last_committed_tag"'/g" snapcraft.yaml
+fi
 
 snap() {
     snapcraft clean
