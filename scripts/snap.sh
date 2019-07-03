e8724df5afb1e7d42a44e12365bc099215d9adfa
Leo Arias <yo@elopio.net>
2019-07-03 15:54:27 -0600

    Use GOPROXY
    As suggested in https://github.com/dgraph-io/badger/issues/886#issuecomment-505500955

--- scripts/snap.sh
+++ scripts/snap.sh
@@ -23,6 +23,7 @@ snap() {
     ARCH_TRIPLET=$1 TARGET_GOARCH=$2 snapcraft --target-arch $3
 }
 
+export GOPROXY='https://proxy.golang.org'
 snap x86_64-linux-gnu amd64 amd64
 snap arm-linux-gnueabihf arm armhf
 snap aarch64-linux-gnu arm64 arm64
