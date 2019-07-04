fc1db0e06ac5bb58d20d1a0c8e9c460be444447b
Leo Arias <yo@elopio.net>
2019-07-04 17:14:30 -0600

    remove gomodule

--- scripts/snap.sh
+++ scripts/snap.sh
@@ -23,7 +23,6 @@ snap() {
     ARCH_TRIPLET=$1 TARGET_GOARCH=$2 snapcraft --target-arch $3
 }
 
-export GO111MODULE=on
 snap x86_64-linux-gnu amd64 amd64
 snap arm-linux-gnueabihf arm armhf
 snap aarch64-linux-gnu arm64 arm64
