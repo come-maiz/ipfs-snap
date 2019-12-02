490597b3d7530e8ab1998ce37eae9533f262db59
Leo Arias <elopio@openzeppelin.com>
2019-12-02 01:04:09 +0000

    Fix the snapcraft script

--- scripts/snap.sh
+++ scripts/snap.sh
@@ -18,5 +18,4 @@ if [ "${last_committed_tag}" != "${last_released_tag}" ]; then
     sed -i "s/version:.*$/version: '"$last_committed_tag"'/g" snapcraft.yaml
 fi
 
-sudo rm -rf parts
-snapcraft clean
+snapcraft
